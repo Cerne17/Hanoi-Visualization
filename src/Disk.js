4425c40f6b6310f9d6aaa6b9204317f89a44ec80
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2024-09-16 18:05:27 -0300

    feat: let the disks have different colors

--- src/Disk.js
+++ src/Disk.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+const diskColors = ['#FF5733', '#33FF57', '#3357FF', '#F3FF33', '#FF33F6']
+
 const Disk = ({ size }) => {
   const width = 20 + size * 20; // Disk width is 20px + 20px per disk size
-  return <div className="disk" style={{ width: `${width}px` }} />;
+  const color = diskColors[size - 1];
+  return <div className="disk" style={{ width: `${width}px`, backgroundColor: color }} />;
 };
 
 export default Disk;
