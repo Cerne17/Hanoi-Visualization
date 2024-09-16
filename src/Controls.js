fdb74a1c6a63a107473c2960486f22e8f05d0c5f
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2024-09-16 18:10:20 -0300

    feat: now the user can choose the amount of disks

--- src/Controls.js
+++ src/Controls.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const Controls = ({ handleSolve, handleReset, isSolving }) => {
+const Controls = ({ handleSolve, handleReset, isSolving, setNumDisks, numDisks }) => {
   return (
     <div className="controls">
+      <label>
+        Number of Disks:
+        <input
+          type="number"
+          min="1"
+          max="5"
+          value={numDisks}
+          onChange={(event) => setNumDisks(Number(event.target.value))}
+          disabled={isSolving}
+        />
+      </label>
       <button onClick={handleSolve} disabled={isSolving}>Solve</button>
       <button onClick={handleReset}>Reset</button>
     </div>
